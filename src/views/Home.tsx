edf699aa45949fa67af9ece4e304b60fd6b54fc8
ToastBubbles <jeffneal11@gmail.com>
2024-03-25 14:49:07 -0500

    added router

---
+++ src/views/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-100 new-container">
+        <div className="jumbotron">home</div>
+      </div>
+    </>
+  );
+}
---
+++ src/views/NotFound.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="w-100 new-container">
+        <div className="jumbotron">not found</div>
+      </div>
+    </>
+  );
+}
